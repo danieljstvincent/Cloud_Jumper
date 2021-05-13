6b966f28c378c06d04a69995dbf9184a40901ab0
Daniel St. Vincent <59455549+danieljstvincent@users.noreply.github.com>
2021-05-13 13:24:54 -0700

    VR and Desktop Ver
    Entire FIle

---
+++ Cloud_Runer-001/Assets/Destruccion.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destruccion : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag.Equals("DestrucCol"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+    void OnTriggerEnter(Collider other)
+        {
+        if(other.gameObject.tag.Equals("DestrucTrig"))
+        {
+            Destroy(other.gameObject);
+        }
+        }
+
+    }
+
